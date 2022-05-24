e0e6b422acee19ebb87767ad1c9ec15ba530cb36
Danny Zolp <danny@zolp.dev>
2022-05-23 20:01:53 -0500

    feat: :zap: automatically reload

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
+import { Watch } from "react-loader-spinner";
 
 function App() {
+  const [loading, setLoading] = useState(true);
   const [price, setPrice] = useState(0);
   const [change, setChange] = useState(0);
 
   const fetchData = () => {
+    setLoading(true);
     fetch(
       `https://www.cmegroup.com/CmeWS/mvc/Quotes/ContractsByNumber?productIds=22&contractsNumber=1&venue=G&type=VOLUME&isProtected=&_t=${new Date().getTime()}`
     )
@@ -12,13 +15,21 @@ function App() {
       .then((json) => {
         setPrice(parseFloat(json[0].last as string));
         setChange(parseFloat(json[0].change as string));
+        setLoading(false);
       });
   };
 
   useEffect(fetchData, []);
 
+  setInterval(fetchData, 6000);
+
   return (
     <main>
+      <div style={{ position: "absolute", top: "0", left: "25px" }}>
+        {loading ? (
+          <Watch color="white" ariaLabel="loading-cows" width="2rem" />
+        ) : null}
+      </div>
       <h1>Future Cows</h1>
       <h2>
         <code className={change > 0 ? "positive" : "negative"}>${price}</code>
